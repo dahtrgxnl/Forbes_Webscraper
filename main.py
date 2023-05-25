8230802c7e3ad2204aed18527d5e47dbdf5e3775
dahtrgxnl <thelandisarnolds@gmail.com>
2023-05-25 14:33:03 -0400

    Updates the info gathering

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 # Set up a Selenium driver
-service = Service('your-chrome-driver')
+service = Service('/Users/alexanderlandis-arnold/Downloads/chromedriver_mac64/chromedriver')
 driver = webdriver.Chrome(service=service)
 
 # Open the URL
-url = "https://milton.nutrislice.com/menu/forbes/dinner/2023-05-22"
+url = "https://milton.nutrislice.com/menu/forbes/dinner"
 driver.get(url)
 
-#Clicks the button to continue to the menu screen
+# Clicks the button to continue to the menu screen
 button = driver.find_element("css selector", ".primary")
 button.click()
 
-#Waits for the page to load
+# Waits for the page to load
 import time
+
 time.sleep(10)
 
 # Get the page's HTML
@@ -24,15 +25,14 @@ html = driver.page_source
 # Parse the HTML with BeautifulSoup
 soup = BeautifulSoup(html, 'html.parser')
 
-# Find the menu items in the parsed HTML
-menu_items = soup.find_all('div', class_='food-name-container')
+date = soup.find_all('div', class_='day-label-wrapper')
+menu = soup.find_all('div', class_='menu-container')
+x = 0
+for i in date:
+   day_menu = str(i.text) + str(menu[x].text)
+   day_menu = day_menu.replace("  ", "\n")
+   print(day_menu)
+   x += 1
 
-for item in menu_items:
-    name = item.find('span', class_='food-name').text
-    print(name)
-
-# Extract the information you want from each menu item
-print(menu_items)
-
-
+# Close the Selenium driver
 driver.quit()
